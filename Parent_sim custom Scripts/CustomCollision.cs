6c65ee26215ad8442ec07d01230c73a583cded77
Aragorn9756 <45053510+Aragorn9756@users.noreply.github.com>
2021-02-08 15:55:45 -0700

    Upload Parenting Simulator Custom Scripts

---
+++ Parent_sim custom Scripts/CustomCollision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomCollision : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("hit" + other.name);
+        if (other.name == "grabbedChair")
+        {
+            other.transform.position = transform.position;
+            other.transform.parent = transform.parent;
+            Debug.Log("Snapped " + other.name + " to " + this.name);
+        }
+    }
+}
---
+++ Parent_sim custom Scripts/Timer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text text;
+    public string winText;
+    public string loseText;
+    public float timeLeft;
+    public bool gameOver = false;
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameOver) {
+            timeLeft -= Time.deltaTime;
+            text.text = "Time Left: " + Mathf.Ceil(timeLeft).ToString();
+
+            //if times up
+            if (timeLeft <= 0)
+            {
+                text.text = winText;
+                gameOver = true;
+                GameManager.Instance.GameOver();
+            }
+        }
+        
+
+    }
+
+    public void GameLost ()
+    {
+        text.text = loseText;
+        gameOver = true;
+    }
+}
